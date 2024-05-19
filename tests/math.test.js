88aa0a31e674d1c49d5aeed053d76eef9f826b49
soumik9 <soumik.ahammed.9@gmail.com>
2024-05-19 22:42:58 +0600

    number string and invalid string pass test

--- tests/math.test.js
+++ tests/math.test.js
@@ -13,3 +13,28 @@ it('should return correct sum if array of numbers provided', () => {
     // assertions
     expect(result).toBe(expectedResult);
 })
+
+it('should return nan if array of numbers provided have any inavalid string', () => {
+
+    // arrange [AAA]
+    const numbers = [1, 'inavalid', 3];
+
+    // Actions
+    const result = add(numbers);
+
+    // assertions
+    expect(result).toBeNaN();
+})
+
+it('should return correct sum if array of number strings provided', () => {
+
+    // arrange [AAA]
+    const numbers = ['1', '2', 3];
+    const expectedResult = numbers.reduce((acc, cur) => +acc + +cur);
+
+    // Actions
+    const result = add(numbers);
+
+    // assertions
+    expect(result).toBe(expectedResult);
+})
