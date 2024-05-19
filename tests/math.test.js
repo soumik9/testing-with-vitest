a151963dbdf31df770b8dbc75c46e591e45e2e66
soumik9 <soumik.ahammed.9@gmail.com>
2024-05-19 22:36:25 +0600

    AAA theorey

--- tests/math.test.js
+++ tests/math.test.js
@@ -2,7 +2,14 @@ import { it, expect } from 'vitest'
 import { add } from '..'
 
 it('should return correct sum if array of numbers provided', () => {
-    const result = add([1, 2, 3]);
 
-    expect(result).toBe(6)
+    // arrange [AAA]
+    const numbers = [1, 2, 3];
+    const expectedResult = numbers.reduce((acc, cur) => acc + cur);
+
+    // Actions
+    const result = add(numbers);
+
+    // assertions
+    expect(result).toBe(expectedResult);
 })
